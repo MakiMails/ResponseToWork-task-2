12e5518474e2fab34bec39c2ca8a754618888042
MakiMails <102325265+MakiMails@users.noreply.github.com>
2022-12-13 22:05:09 +0300

    Add files via upload

---
+++ Work2/Product.cs
@@ -0,0 +1,6 @@
+﻿internal class Product
+{
+    public int ID{get; set;}
+    public string NameProduct{ get; set;}
+    public string Сategory { get; set;}
+}
---
+++ Work2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+//Здравствуйте
+//Размыто написано ТЗ второго задания, так как не понят, как должна выглядить БД.
+//Одному продукту может соответствовать много категорий, в одной категории может быть много продуктов.
+//Что под этим подразумевается?
+//Есть много вариантов, как это понять.
+//Будет в БД одна таблица, где будет поля такие как название продукта и категория
+//Или же будет две таблицы: Продукты(поля название продукта и категория) и Категория(категория и название продукта)
+//Пожалуйста, отпишите https://t.me/MakiMails готов буду исправить программу, которая подойдет вашим условиям
+//Спасибо за просмотр выполненного задания
+
+
+SqlConnection sqlConnection = ConectDB();
+
+if(sqlConnection != null)
+{
+    if(sqlConnection.State == ConnectionState.Open)
+    {
+        var products = sqlConnection.Query<Product>("SELECT * FROM Products").ToList();
+        if(products.Count > 0)
+        {
+            foreach(var product in products)
+            {
+                Console.Write($"{product.NameProduct} ");
+                Console.WriteLine(product.Сategory);
+            }
+        }
+    }
+    else
+    {
+        throw new Exception("Close BD");
+    }
+}
+else
+{
+    throw new Exception("Not conect BD");
+}
+
+SqlConnection ConectDB()
+{
+    try
+    {
+        //Для запуска программы нужно указать путь к БД
+        SqlConnection sqlConnection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\3 курс\\works\\Work2\\Work2\\DatabaseProducts.mdf\";Integrated Security=True");
+        sqlConnection.Open();
+        return sqlConnection;
+    }
+    catch
+    {
+        Console.WriteLine("Неверый путь к подключению БД");
+        return null;
+    }
+}
+
